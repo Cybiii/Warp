f067e1a59e6a9a484ad4ea2a097eb3140c91ac6c
SILVA PIZARRO <cs199-akc@eda-1.EECS.Berkeley.EDU>
2026-01-28 20:20:23 -0800

    chisel blackbox wrapper

--- chipyard/WarpAccelerator.scala
+++ chipyard/WarpAccelerator.scala
@@ -1,19 +1,138 @@
-// Chipyard Accelerator Wrapper
-// TODO: Implement Chisel BlackBox wrapper for Verilog warp engine
-// - Wrap Verilog module in Chisel BlackBox
-// - Connect RoCC interface to core
-// - Handle parameter passing (NUM_LANES, etc.)
+// Chipyard Accelerator Wrapper for Warp SIMT Engine
+// Wraps SystemVerilog warp_engine module as a RoCC accelerator
 
 package warp
 
 import chisel3._
 import chisel3.util._
-import freechips.rocketchip.config.Parameters
+import chisel3.experimental.{IntParam}
+import freechips.rocketchip.config.{Parameters, Field}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tile._
+import freechips.rocketchip.rocket.{TLBConfig}
 
-// TODO: Implement WarpAccelerator class
-// TODO: Extend RoCCAccelerator
-// TODO: Create BlackBox for Verilog module
-// TODO: Connect RoCC interface signals
+// Configuration parameters for Warp engine
+case class WarpParams(
+  numLanes: Int = 8,
+  dataWidth: Int = 32,
+  regFileSize: Int = 32,
+  fifoDepth: Int = 16,
+  addrWidth: Int = 32
+)
+
+case object WarpKey extends Field[Option[WarpParams]](None)
+
+// BlackBox wrapper for SystemVerilog warp_engine module
+class WarpEngineBlackBox(params: WarpParams) extends BlackBox(Map(
+  "NUM_LANES" -> IntParam(params.numLanes),
+  "DATA_WIDTH" -> IntParam(params.dataWidth),
+  "REG_FILE_SIZE" -> IntParam(params.regFileSize),
+  "FIFO_DEPTH" -> IntParam(params.fifoDepth),
+  "ADDR_WIDTH" -> IntParam(params.addrWidth)
+)) with HasBlackBoxResource {
+  
+  val io = IO(new Bundle {
+    val clk = Input(Clock())
+    val rst_n = Input(Bool())
+    
+    // RoCC Command interface
+    val cmd_valid = Input(Bool())
+    val cmd_ready = Output(Bool())
+    val cmd_funct = Input(UInt(7.W))
+    val cmd_rs1 = Input(UInt(5.W))
+    val cmd_rs2 = Input(UInt(5.W))
+    val cmd_rd = Input(UInt(5.W))
+    val cmd_rs1_data = Input(UInt(32.W))
+    val cmd_rs2_data = Input(UInt(32.W))
+    
+    // RoCC Response interface
+    val resp_valid = Output(Bool())
+    val resp_ready = Input(Bool())
+    val resp_rd = Output(UInt(5.W))
+    val resp_data = Output(UInt(32.W))
+    
+    // RoCC Memory interface
+    val mem_req_valid = Output(Bool())
+    val mem_req_ready = Input(Bool())
+    val mem_req_addr = Output(UInt(params.addrWidth.W))
+    val mem_req_write = Output(Bool())
+    val mem_req_data = Output(UInt(32.W))
+    val mem_resp_valid = Input(Bool())
+    val mem_resp_ready = Output(Bool())
+    val mem_resp_data = Input(UInt(32.W))
+  })
+  
+  // Add SystemVerilog source files
+  addResource("/rtl/warp_engine.sv")
+  addResource("/rtl/rocc_interface.sv")
+  addResource("/rtl/warp_controller.sv")
+  addResource("/rtl/instruction_fifo.sv")
+  addResource("/rtl/warp_mask.sv")
+  addResource("/rtl/lane_array.sv")
+  addResource("/rtl/processing_lane.sv")
+  addResource("/rtl/register_file.sv")
+  addResource("/rtl/alu.sv")
+  addResource("/rtl/memory_interface.sv")
+  addResource("/include/warp_pkg.sv")
+}
+
+// RoCC Accelerator wrapper
+class WarpAccelerator(opcodes: OpcodeSet, params: WarpParams)(implicit p: Parameters)
+  extends LazyRoCC(opcodes) {
+  
+  override lazy val module = new WarpAcceleratorModule(this, params)
+}
+
+class WarpAcceleratorModule(outer: WarpAccelerator, params: WarpParams)(implicit p: Parameters)
+  extends LazyRoCCModuleImp(outer) {
+  
+  // Instantiate the BlackBox
+  val warp_engine = Module(new WarpEngineBlackBox(params))
+  
+  // Connect clock and reset
+  warp_engine.io.clk := clock
+  warp_engine.io.rst_n := !reset.asBool
+  
+  // Connect RoCC command interface
+  warp_engine.io.cmd_valid := io.cmd.valid
+  io.cmd.ready := warp_engine.io.cmd_ready
+  warp_engine.io.cmd_funct := io.cmd.bits.inst.funct
+  warp_engine.io.cmd_rs1 := io.cmd.bits.inst.rs1
+  warp_engine.io.cmd_rs2 := io.cmd.bits.inst.rs2
+  warp_engine.io.cmd_rd := io.cmd.bits.inst.rd
+  warp_engine.io.cmd_rs1_data := io.cmd.bits.rs1
+  warp_engine.io.cmd_rs2_data := io.cmd.bits.rs2
+  
+  // Connect RoCC response interface
+  io.resp.valid := warp_engine.io.resp_valid
+  warp_engine.io.resp_ready := io.resp.ready
+  io.resp.bits.rd := warp_engine.io.resp_rd
+  io.resp.bits.data := warp_engine.io.resp_data
+  
+  // Connect memory interface
+  io.mem.req.valid := warp_engine.io.mem_req_valid
+  warp_engine.io.mem_req_ready := io.mem.req.ready
+  io.mem.req.bits.addr := warp_engine.io.mem_req_addr
+  io.mem.req.bits.tag := 0.U
+  io.mem.req.bits.cmd := Mux(warp_engine.io.mem_req_write, 1.U, 0.U) // 1=write, 0=read
+  io.mem.req.bits.size := 2.U // 32-bit (4 bytes)
+  io.mem.req.bits.signed := false.B
+  io.mem.req.bits.data := warp_engine.io.mem_req_data
+  io.mem.req.bits.phys := false.B
+  io.mem.req.bits.dprv := 0.U
+  
+  warp_engine.io.mem_resp_valid := io.mem.resp.valid
+  warp_engine.io.mem_resp_data := io.mem.resp.bits.data
+  warp_engine.io.mem_resp_ready := true.B // Always ready to accept responses
+  
+  // Handle memory s2_nack
+  io.mem.s1_kill := false.B
+  io.mem.s2_kill := false.B
+  
+  // No exceptions from this accelerator
+  io.interrupt := false.B
+  
+  // Busy signal
+  io.busy := warp_engine.io.cmd_valid && !warp_engine.io.cmd_ready
+}
 
--- chipyard/WarpConfigs.scala
+++ chipyard/WarpConfigs.scala
@@ -1,15 +1,92 @@
-// Chipyard Configuration Files
-// TODO: Implement Chipyard configs for different cores
-// - WithRocketWarpAccelerator
-// - WithBOOMWarpAccelerator
-// - WithCVA6WarpAccelerator
+// Chipyard Configuration Files for Warp Accelerator
+// Provides configuration mixins to add Warp to different cores
 
 package warp
 
-import freechips.rocketchip.config.Config
+import chisel3._
+import org.chipsalliance.cde.config.{Config, Field, Parameters}
 import freechips.rocketchip.subsystem._
+import freechips.rocketchip.tile._
+import freechips.rocketchip.rocket._
 
-// TODO: Implement WithRocketWarpAccelerator config
-// TODO: Implement WithBOOMWarpAccelerator config
-// TODO: Implement WithCVA6WarpAccelerator config
+/**
+ * Adds Warp accelerator to Rocket cores in the system
+ * Usage: new Config(new WithWarpAccelerator ++ ...)
+ */
+class WithWarpAccelerator(
+  numLanes: Int = 8,
+  dataWidth: Int = 32,
+  regFileSize: Int = 32,
+  fifoDepth: Int = 16
+) extends Config((site, here, up) => {
+  case WarpKey => Some(WarpParams(
+    numLanes = numLanes,
+    dataWidth = dataWidth,
+    regFileSize = regFileSize,
+    fifoDepth = fifoDepth,
+    addrWidth = 32
+  ))
+  case BuildRoCC => up(BuildRoCC) ++ Seq(
+    (p: Parameters) => {
+      val warp = LazyModule(new WarpAccelerator(
+        OpcodeSet.custom0,
+        p(WarpKey).get
+      )(p))
+      warp
+    }
+  )
+})
+
+/**
+ * Adds Warp with 8 lanes (default configuration)
+ */
+class WithWarp8Lanes extends WithWarpAccelerator(numLanes = 8)
+
+/**
+ * Adds Warp with 16 lanes (maximum configuration)
+ */
+class WithWarp16Lanes extends WithWarpAccelerator(numLanes = 16)
+
+/**
+ * Adds Warp with 4 lanes (minimal configuration for testing)
+ */
+class WithWarp4Lanes extends WithWarpAccelerator(numLanes = 4)
+
+/**
+ * Complete RocketConfig with Warp 8-lane accelerator
+ * Usage in Chipyard: make CONFIG=RocketWarp8Config
+ */
+class RocketWarp8Config extends Config(
+  new WithWarp8Lanes ++
+  new freechips.rocketchip.system.DefaultConfig
+)
+
+/**
+ * Complete RocketConfig with Warp 16-lane accelerator
+ * Usage in Chipyard: make CONFIG=RocketWarp16Config
+ */
+class RocketWarp16Config extends Config(
+  new WithWarp16Lanes ++
+  new freechips.rocketchip.system.DefaultConfig
+)
+
+/**
+ * Minimal test configuration with Warp 4-lane accelerator
+ * Usage in Chipyard: make CONFIG=RocketWarp4Config
+ */
+class RocketWarp4Config extends Config(
+  new WithWarp4Lanes ++
+  new freechips.rocketchip.system.DefaultConfig
+)
+
+/**
+ * Warp with Rocket in a tiny configuration (for faster simulation)
+ * Usage in Chipyard: make CONFIG=TinyRocketWarpConfig
+ */
+class TinyRocketWarpConfig extends Config(
+  new WithWarp8Lanes ++
+  new WithNSmallCores(1) ++
+  new WithNBanks(1) ++
+  new freechips.rocketchip.system.DefaultConfig
+)
 
